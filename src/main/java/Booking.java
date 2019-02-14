34d2d69484bebd787bc54f8d631962ab20f931a7
Rick Symington <rick.symington@gmail.com>
2019-02-14 09:58:32 +0000

    Add hotel bill

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+
 
 public class Booking {
+
+private Bedroom bedroom;
+private int numberOfnights;
+
+public Booking(Bedroom bedroom, int numberOfnights){
+    this.bedroom = bedroom;
+    this.numberOfnights = numberOfnights;
+}
+
+public double getBill(){
+    return this.numberOfnights * bedroom.getNightlyRate();
+}
+
 }
--- src/test/java/BookingTest.java
+++ src/test/java/BookingTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookingTest {
+
+    Booking booking;
+    Bedroom bedroom;
+
+    @Before
+
+    public void before(){
+        bedroom = new Bedroom(17, "Suite", 4, 155.50);
+        booking = new Booking (bedroom, 3);
+    }
+
+    @Test
+    public void testHotelBill(){
+        assertEquals(466.50, booking.getBill(), 0.01);
+    }
+
+
 }
